5aa8e2738f48574215ec5f5383b3e13e7b813324
unknown <budiarto.salim91@gmail.com>
2021-08-26 21:47:27 +0700

    fixed unit test's missing dotenv dependency

--- 2-express_backend/frameworks/web/express/routes/index.js
+++ 2-express_backend/frameworks/web/express/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const MovieAdapter = require('../adapters/movie-adapter');
 
-router.use('/detail/:movieId', MovieAdapter.getMovieBySearch);
 router.get('/search', MovieAdapter.getMovieBySearch);
+router.get('/detail/:movieId', MovieAdapter.getMovieBySearch);
 
 module.exports = router;
